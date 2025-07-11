a41bb67d016d39ada4a3d3102c400bd477d666d6
Guram Elkanishvili <ze156324829@gmail.com>
2025-07-11 23:11:46 +0400

    init

---
+++ Credo.Core.FileStorage/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+using Credo.Core.FileStorage.Storage;
+using Credo.Core.Minio.DI;
+using Credo.Core.Minio.Storage;
+using Microsoft.Data.SqlClient;
+
+namespace Credo.Core.FileStorage;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddFileStorage(this IServiceCollection services, string connectionString, CredoMinioStorageConfiguration configuration)
+    {
+        services.AddMinioStorage(configuration);
+
+        services.AddScoped<IDbConnection>(sp => new SqlConnection(connectionString));
+        services.AddScoped<UnitOfWork>();
+        services.AddScoped<IFileStorage, FileStorage1>(sp =>
+        {
+            var minioStorage = sp.GetRequiredService<IMinioStorage>();
+            Func<UnitOfWork> uowFactory = () => sp.GetRequiredService<UnitOfWork>();
+            return new FileStorage1(minioStorage, uowFactory);
+        });
+
+        return services;
+    }
+}
---
+++ Credo.Core.Minio/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Credo.Core.Minio.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Credo.Core.Minio;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddCredoCoreMinio(this IServiceCollection services)
+    {
+        services.AddScoped<IMinioStorage, MinioStorage>();
+    }
+}
