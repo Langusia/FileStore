08485b1c6ff4cef72b15c62b0ba49cfd91ab8280
Guram Elkanishvili <ze156324829@gmail.com>
2025-09-15 16:24:53 +0400

    init migration worker

--- Api/Credo.FileStorage/src/Credo.FileStorage.Worker/Program.cs
+++ Api/Credo.FileStorage/src/Credo.FileStorage.Worker/Program.cs
@@ -1,4 +1,6 @@
 using Credo.FileStorage.Application;
+using Credo.Core.FileStorage;
+using Credo.Core.Minio.DI;
 using Credo.FileStorage.Worker;
 using Credo.FileStorage.Worker.Configuration;
 using Credo.JCS.Extension.Services;
@@ -13,6 +15,11 @@ builder.Services.AddHealthChecks(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddRepositories();
 builder.Services.AddServices();
+
+// Wire FileStorage library with DB and MinIO configuration
+var minioConfig = builder.Configuration.GetSection("Minio").Get<CredoMinioStorageConfiguration>();
+var documentDb = builder.Configuration.GetConnectionString("DocumentDb");
+builder.Services.AddFileStorage(documentDb!, minioConfig!);
 
 var host = builder.Build();
 host.Run();
